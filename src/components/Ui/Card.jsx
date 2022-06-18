ab2e4cfcf40daf8391c8eefb3e52710d5d514169
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-18 17:27:28 +0430

    [BUGFIXED] change bg of card

--- src/components/Ui/Card.jsx
+++ src/components/Ui/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({children}) {
   return (
-    <div className='p-4  border border-gray-200 rounded'>{children}</div>
+    <div className='p-4 bg-white border border-gray-200 rounded'>{children}</div>
   )
 }
 
