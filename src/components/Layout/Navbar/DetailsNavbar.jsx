ddee36229b9b9d4c7f0118520816ef60b49970ef
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-18 14:36:41 +0430

    [IMPROVMENT] change background of details nav

--- src/components/Layout/Navbar/DetailsNavbar.jsx
+++ src/components/Layout/Navbar/DetailsNavbar.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const DetailsNavbar = () => {
   return (
-    <header className='flex items-center justify-between px-4 py-3 bg-gradient-to-b from-blue-300 to-blue-100 text-white'>
+    <header className='flex items-center justify-between px-4 py-3 bg-gradient-to-b from-blue-500 to-blue-100 text-white'>
         <Link to="/" className='bg-blue-500 rounded p-2 py-1'>back</Link>
     </header>
   )
