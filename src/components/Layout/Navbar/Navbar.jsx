fd1ff745e2b47ab2fda22c8837f1904b3bd55b61
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-18 13:48:35 +0430

    [IMPROVMNET] adding Default navbar

---
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <header className="flex items-center justify-between p-4 bg-gradient-to-b from-blue-500 to-blue-300 text-white">
+      <h1 className="text-center">post list</h1>
+    </header>
+  );
+}
+
+export default Navbar;
