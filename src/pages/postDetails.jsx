0c3c423602a23cc5f12e9093a7a9971b681bfc3b
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-18 14:37:36 +0430

    [FEATURE] finish posts details

--- src/pages/postDetails.jsx
+++ src/pages/postDetails.jsx
@@ -1,9 +1,49 @@
-const PostDetailsPage = function (){
-    return (
-        <div>
-            post Detail page
-        </div>
-    )
-}
+import Layout from "../components/Layout/Layout";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "../components/Ui/Card";
+import Loading from "../components/Ui/Loading";
 
-export default PostDetailsPage
+const PostDetailsPage = function () {
+  const params = useParams();
+  const [post, setPost] = useState(null);
+  const [requestStatus, setRequestStatus] = useState({
+    loading: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    setRequestStatus({ loading: true, error: null });
+    axios
+      .get(
+        `https://jsonplaceholder.typicode.com/posts/${params.id}
+            `
+      )
+      .then((response) => {
+        setPost(response.data || null);
+        setRequestStatus({ loading: false, error: null });
+      })
+      .catch((error) => {
+        setRequestStatus({ loading: false, error: error.message });
+        setPost(null);
+      });
+  }, []);
+
+  return (
+    <Layout mode="postDetail">
+      {requestStatus.loading && <Loading/>}
+      {requestStatus.error && <p>{requestStatus.error}</p>}
+      {post && !requestStatus.loading && (
+        <Card>
+          <div className="flex flex-col space-y-2">
+            <h1 className="font-medium">{post.title}</h1>
+            <p className="text-sm">{post.body}</p>
+          </div>
+        </Card>
+      )}
+    </Layout>
+  );
+};
+
+export default PostDetailsPage;
