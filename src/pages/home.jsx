9336d5dd20a8ae93f748b664d2a2ac339c40a72b
Amirreza Torangan <amir.torangan@gmail.com>
2022-06-18 14:20:35 +0430

    [FEATURE] posts list page completed

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,50 @@
-const HomeDetailsPage = function (){
-    return (
-        <div>
-            Home Detail page
-        </div>
-    )
-}
+import { useEffect, useState } from "react";
+import Layout from "../components/Layout/Layout";
+import axios from "axios";
+import Pagination from "../components/Ui/pagination";
+import Posts from "../components/Pages/home/Posts";
+const HomeDetailsPage = function () {
+  const [posts, setPosts] = useState(null);
+  const [requestStatus, setRequestStatus] = useState({
+    loading: false,
+    error: null,
+  });
+  const [pageNumber, setPageNumber] = useState(1);
 
-export default HomeDetailsPage
+  useEffect(() => {
+    setRequestStatus({ loading: true, error: null });
+    axios
+      .get(
+        `https://jsonplaceholder.typicode.com/posts?_page=${pageNumber}&_limit=20
+        `
+      )
+      .then((response) => {
+        setPosts(response.data || null);
+        setRequestStatus({ loading: false, error: null });
+      })
+      .catch((error) => {
+        setRequestStatus({ loading: false, error: error.message });
+        setPosts(null);
+      });
+  }, [pageNumber]);
+
+  return (
+    <Layout>
+      <div>Home Detail page</div>
+      {posts && !requestStatus.loading && (
+        <>
+          <Posts data={posts} />
+          <Pagination
+            pageNumber={pageNumber}
+            setPageNumber={setPageNumber}
+            totalAmount={100 / 20}
+          />
+        </>
+      )}
+      {requestStatus.loading && <p>loading...</p>}
+      {requestStatus.error && <p className="text-red-500 text-center">{requestStatus.error}</p>}
+    </Layout>
+  );
+};
+
+export default HomeDetailsPage;
